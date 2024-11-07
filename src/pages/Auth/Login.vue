e16a841dbd69715b40b522d336ff6151ba94a33e
Jeferson Conceição de Oliveira <jeferson.oliveira@salvador.ba.gov.br>
2024-11-07 15:14:52 -0300

    first commit

---
+++ src/pages/Auth/Login.vue
@@ -0,0 +1,16 @@
+<script setup>
+import GuestLayout from '@/pages/Layout/GuestLayout.vue';
+</script>
+
+<template>
+  <GuestLayout>
+    <div>
+      <h1> Login </h1>
+      <div>
+        <label> E-mail </label>
+        <label> Senha </label>
+      </div>
+
+    </div>
+  </GuestLayout>
+</template>
